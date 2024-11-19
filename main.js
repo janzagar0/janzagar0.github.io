aa9a5a2d8d699539c0d0d4df4667778a660b3c12
janzagar0 <163489445+janzagar0@users.noreply.github.com>
2024-11-18 22:18:21 -0500

    Adding new webpage files.

---
+++ main.js
@@ -0,0 +1,21 @@
+// Loader
+const loader = document.getElementById("loader");
+const loaderPage = document.getElementById("loader-page");
+
+function timeLoader() {
+    loaderTimer = setTimeout(hideLoader, 2500);
+}
+
+function hideLoader() {
+    loader.style.display = "none";
+    loaderPage.style.display = "none";
+}
+
+function hamburgerMenu() {
+    var x = document.getElementById("mobile-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
